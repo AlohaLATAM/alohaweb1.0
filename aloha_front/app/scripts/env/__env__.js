d6e7ab57d70e1db2e3efd0ecaaf727972ad92dc5
Lou <hola@loualcala.com>
2018-03-18 17:14:32 -0500

    rename

---
+++ aloha_front/app/scripts/env/__env__.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * const __author__ = 'loualcala'
+ * const __copyright__ = 'Copyright (c) 2018+ aloha. All Rights Reserved'
+ *
+ **/
+
+(function () {
+
+    'use strict';
+
+    window.__env__ = {
+        // API_ROOT: 'http://localhost:8000',
+        API_ROOT: 'https://panel.aloha.pe',
+        HTML5MODE: false
+    };
+
+}());
