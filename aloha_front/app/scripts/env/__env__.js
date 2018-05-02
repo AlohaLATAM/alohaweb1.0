f1c23224ef88e5f72d6f42fce897de7401c9f3e2
Lou <hola@loualcala.com>
2018-05-02 12:12:32 -0500

    add design changes

--- aloha_front/app/scripts/env/__env__.js
+++ aloha_front/app/scripts/env/__env__.js
@@ -11,8 +11,8 @@
 
     window.__env__ = {
         // API_ROOT: 'http://localhost:8000',
-        // API_ROOT: 'https://panel.aloha.pe',
-        API_ROOT: '',
+        API_ROOT: 'https://panel.aloha.pe',
+        // API_ROOT: '',
         HTML5MODE: false
     };
 
