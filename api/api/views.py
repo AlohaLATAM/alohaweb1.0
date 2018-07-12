421d7608cb6405cbf042cfa48fd960bd72c6fb56
Lou <hola@loualcala.com>
2018-07-12 17:24:49 -0500

    upload landing page design progress

--- api/api/views.py
+++ api/api/views.py
@@ -1,6 +1,19 @@
-# -*- coding: utf-8 -*-
+import datetime
+
 from django.shortcuts import render_to_response
 
 
+now = datetime.datetime.now()
+
+
+def landing(request):
+    return render_to_response('base.html', {
+        'page': 'views/home.html',
+        'title': 'Aloha',
+        'description': 'Description',
+        'current_year': now.year,
+    })
+
+
 def index(request):
-    return render_to_response('index.html');
+    return render_to_response('index.html')
